adf6c95c73bd4a434b8caae05aa719b6b5204573
Louis Ta <louistaa@gmail.com>
2020-10-20 15:50:45 -0700

    Updates

---
+++ precache-manifest.f71ac9ab55f00a4315548a957196787b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ad878c0d87c7db45258b3fafcf7f4b5",
+    "url": "/Louis_Ta_Zillow_Report/index.html"
+  },
+  {
+    "revision": "9f8c54798ef028686a85",
+    "url": "/Louis_Ta_Zillow_Report/static/css/2.ad510f05.chunk.css"
+  },
+  {
+    "revision": "483c5537f0b7c740460e",
+    "url": "/Louis_Ta_Zillow_Report/static/css/main.08817f44.chunk.css"
+  },
+  {
+    "revision": "9f8c54798ef028686a85",
+    "url": "/Louis_Ta_Zillow_Report/static/js/2.7ed22477.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Louis_Ta_Zillow_Report/static/js/2.7ed22477.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "483c5537f0b7c740460e",
+    "url": "/Louis_Ta_Zillow_Report/static/js/main.650fe233.chunk.js"
+  },
+  {
+    "revision": "0a599e079e9b5ca2ab4e",
+    "url": "/Louis_Ta_Zillow_Report/static/js/runtime-main.dc1d7f0f.js"
+  }
+]);
